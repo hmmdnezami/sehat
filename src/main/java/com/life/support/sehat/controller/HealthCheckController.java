5ec45a88e71d9c94810452e22dfd190eed275ac2
waris8 <gwaris98@gmail.com>
2024-06-20 20:17:40 +0530

    added healthcheck

---
+++ src/main/java/com/life/support/sehat/controller/HealthCheckController.java
@@ -0,0 +1,15 @@
+package com.life.support.sehat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test")
+public class HealthCheckController {
+
+    @GetMapping(value = "/hello")
+    public String helloWorld(){
+        return "Hello World";
+    }
+}
